7e1d4074c289ac40fa78978cedee0d037457a1c8
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-08-10 14:00:52 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,15 +1,24 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int total = brown + yellow;
-
-        for (int height = 3; height <= total / height; height++) {
-            int width = total / height;
-
-            if ((width - 2) * (height - 2) == yellow) {
-                return new int[]{width, height};
+        int height;
+        int width;
+        
+        for(int i = 1; ; i++) {
+            height = i;
+            if(yellow % height != 0) {
+                continue;
+            }
+            width = yellow / height;
+            
+            if((width + 2) * (height + 2) == brown + yellow) {
+                break;
             }
         }
-
-        return new int[]{};
+        
+        int[] result = new int[2];
+        result[0] = width + 2;
+        result[1] = height + 2;
+        
+        return result;
     }
 }
