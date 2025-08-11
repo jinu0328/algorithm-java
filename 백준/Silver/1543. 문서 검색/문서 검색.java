6aa06e0b7076624cc001b00ec46c834c8727b523
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-08-12 00:06:58 +0900

    [Silver V] Title: 문서 검색, Time: 196 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -4,35 +4,20 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        char[] document = scanner.nextLine().toCharArray();
-        char[] word = scanner.nextLine().toCharArray();
-        int count = 0;
+        String document = scanner.nextLine();
+        String word = scanner.nextLine();
 
-        for(int i = 0; i < document.length; i++) {
-            if(document[i] == word[0]) {
-                int originI = i;
-                boolean matched = true;
-                for(int j = originI + 1; j < originI + word.length; j++) {
-                    i++;
-                    if(j >= document.length) {
-                        matched = false;
-                        break;
-                    }
-                    if(document[j] != word[j - originI]) {
-                        matched = false;
-                        break;
-                    }
-                }
-                if(matched) {
-                    count++;
-                }
-                else {
-                    i = originI;
-                }
+        int i = 0;
+        int count = 0;
+        while (true) {
+            int index = document.indexOf(word, i);
+            if(index >= 0) {
+                count++;
+                i = index + word.length();
             }
+            else break;
         }
 
         System.out.println(count);
-
     }
 }
