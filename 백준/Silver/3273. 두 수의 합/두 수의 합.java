097eea6210062857e72089a43401483758f297fb
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-08-22 00:29:28 +0900

    [Silver III] Title: 두 수의 합, Time: 276 ms, Memory: 34480 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,35 +1,34 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n;
+    static int[] nums;
+    static int x;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n = Integer.parseInt(br.readLine());
-        int[] nums = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        int x = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        x = Integer.parseInt(br.readLine());
 
         int count = 0;
-        int[] counts = new int[1000001];
+        int[] real = new int[2000000];
+
         for(int i = 0; i < n; i++) {
-            counts[nums[i]] = 1;
+            real[nums[i]] = 1;
         }
 
         for(int i = 0; i < n; i++) {
-            int diff = x - nums[i];
-            if(diff < 1 || diff > 1000000) {
+            if(x - nums[i] < 0) {
                 continue;
             }
-            
-            if(counts[diff] != 0) {
+            if(real[x - nums[i]] == 1) {
                 count++;
             }
+
         }
-        bw.write(count/2 + "\n");
-        bw.flush();
+
+        System.out.println(count/2);
     }
 }
