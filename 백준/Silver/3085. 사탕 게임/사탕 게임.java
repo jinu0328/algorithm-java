63509241ab8bd47094b2a0cd3f4a2a3f8e1d255f
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-08-23 23:55:02 +0900

    [Silver II] Title: 사탕 게임, Time: 148 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,67 +1,84 @@
 import java.io.*;
-
+import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N;
+    static char[][] board;
+
 
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        char[][] board = new char[N][N];
-        for (int i = 0; i < N; i++) board[i] = br.readLine().toCharArray();
+         N = Integer.parseInt(br.readLine());
+         board = new char[N][N];
+         for(int i = 0; i < N; i++) {
+             board[i] = br.readLine().toCharArray();
+         }
 
-        int max = getMaxCandy(board); // 원판 자체의 최대도 고려
+         int count = 0;
+         count = Math.max(count, countMax());
 
-        for (int x = 0; x < N; x++) {
-            for (int y = 0; y < N; y++) {
-                // 오른쪽과 교환
-                if (y + 1 < N && board[x][y] != board[x][y + 1]) {
-                    swap(board, x, y, x, y + 1);
-                    max = Math.max(max, getMaxCandy(board));
-                    swap(board, x, y, x, y + 1); // 원복
+         for(int i = 0; i < N; i++) {
+             for(int j = 0; j < N - 1; j++) {
+                 if(board[i][j] == board[i][j + 1]) {
+                     continue;
+                 }
+
+                 char tmp = board[i][j + 1];
+                 board[i][j + 1] = board[i][j];
+                 board[i][j] = tmp;
+                 count = Math.max(countMax(), count);
+                 tmp = board[i][j + 1];
+                 board[i][j + 1] = board[i][j];
+                 board[i][j] = tmp;
+             }
+         }
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N - 1; j++) {
+                if(board[j][i] == board[j + 1][i]) {
+                    continue;
                 }
-                // 아래와 교환
-                if (x + 1 < N && board[x][y] != board[x + 1][y]) {
-                    swap(board, x, y, x + 1, y);
-                    max = Math.max(max, getMaxCandy(board));
-                    swap(board, x, y, x + 1, y); // 원복
-                }
+
+                char tmp = board[j + 1][i];
+                board[j + 1][i] = board[j][i];
+                board[j][i] = tmp;
+                count = Math.max(countMax(), count);
+                tmp = board[j + 1][i];
+                board[j + 1][i] = board[j][i];
+                board[j][i] = tmp;
             }
         }
 
-        bw.write(max + "\n");
-        bw.flush();
+        System.out.println(count);
     }
 
-    private static int getMaxCandy(char[][] b) {
-        int max = 1;
+    private static int countMax() {
+        int count = 0;
 
-        // 가로 검사
-        for (int i = 0; i < N; i++) {
-            int count = 1;
-            for (int j = 1; j < N; j++) {
-                if (b[i][j] == b[i][j - 1]) count++;
-                else count = 1;
-                if (count > max) max = count;
+        for(int i = 0; i < N; i++) {
+            int tmpCount = 1;
+            for(int j = 1; j < N; j++) {
+                if(board[i][j] == board[i][j - 1]) {
+                    tmpCount++;
+                } else {
+                    tmpCount = 1;
+                }
+                count = Math.max(count, tmpCount);
             }
         }
 
-        // 세로 검사
-        for (int j = 0; j < N; j++) {
-            int count = 1;
-            for (int i = 1; i < N; i++) {
-                if (b[i][j] == b[i - 1][j]) count++;
-                else count = 1;
-                if (count > max) max = count;
+        for(int i = 0; i < N; i++) {
+            int tmpCount = 1;
+            for(int j = 1; j < N; j++) {
+                if(board[j][i] == board[j - 1][i]) {
+                    tmpCount++;
+                } else {
+                    tmpCount = 1;
+                }
+                count = Math.max(count, tmpCount);
             }
         }
 
-        return max;
-    }
-
-    private static void swap(char[][] b, int x1, int y1, int x2, int y2) {
-        char tmp = b[x1][y1];
-        b[x1][y1] = b[x2][y2];
-        b[x2][y2] = tmp;
+        return count;
     }
 }
