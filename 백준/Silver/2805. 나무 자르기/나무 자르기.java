f229959eb66e28e0f2e5034bf5d86cdc10c19c46
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-09-01 00:43:39 +0900

    [Silver II] Title: 나무 자르기, Time: 488 ms, Memory: 119348 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -3,46 +3,42 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        sc.nextLine();
+        int N = Integer.parseInt(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
         int[] tree = new int[N];
-        for(int i = 0; i < N; i++) {
-            tree[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        int maxHeight = 0;
+        for (int i = 0; i < N; i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+            if (tree[i] > maxHeight) maxHeight = tree[i];
         }
-        int result = 0;
 
-        Arrays.sort(tree);
+        int low = 0, high = maxHeight, result = 0;
 
-        int low = 0;
-        int high = tree[tree.length - 1];
-
-        while(low <= high) {
+        while (low <= high) {
             int mid = (low + high) / 2;
             long remain = getRemain(tree, mid);
 
-            if(remain < M) {
+            if (remain < M) {
                 high = mid - 1;
-            }
-            else {
+            } else {
+                result = mid; // mid가 정답 후보
                 low = mid + 1;
-                result = Math.max(mid, result);
             }
         }
 
         System.out.println(result);
-
     }
 
     private static long getRemain(int[] tree, int mid) {
         long remain = 0;
-
-        for(int num : tree) {
-            remain += Math.max(0, num - mid);
+        for (int h : tree) {
+            if (h > mid) remain += (h - mid);
         }
-
         return remain;
     }
 }
