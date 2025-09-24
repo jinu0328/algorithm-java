4eca832c840552db4b5e630010b6692d669e30a5
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-09-24 22:54:18 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1240 ms, Memory: 165200 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,43 +1,44 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N, M;
+    static int[][] board;
+
 
     public static void main(String[] args) throws IOException {
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
+      String[] input = br.readLine().split(" ");
+      N = Integer.parseInt(input[0]);
+      M = Integer.parseInt(input[1]);
 
-        int[][] board = new int[N + 1][N + 1];
+      board = new int[N + 1][N + 1];
+      for(int i = 1; i <= N; i++) {
+          board[i] = Arrays.stream(("0 " + br.readLine()).split(" ")).mapToInt(Integer::parseInt).toArray();
+      }
 
-        for(int i = 1; i <= N; i++) {
-            board[i] = Arrays.stream(("0 " + br.readLine()).split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
+      int[][] acc = new int[N + 1][N + 1];
+      for(int i = 1; i <= N; i++) {
+          for(int j = 1; j <= N; j++) {
+              acc[i][j] = acc[i][j - 1] + board[i][j];
+          }
+      }
 
-        int[][] accRow = new int[N + 1][N + 1];
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j <= N; j++) {
-                accRow[i][j] =  accRow[i][j-1] + board[i][j];
-            }
-        }
+      while(M-- > 0) {
+          String[] input2 = br.readLine().split(" ");
+          int x1, x2, y1, y2;
+          x1 = Integer.parseInt(input2[0]);
+          y1 = Integer.parseInt(input2[1]);
+          x2 = Integer.parseInt(input2[2]);
+          y2 = Integer.parseInt(input2[3]);
 
-        while(M-- > 0) {
-            int sum = 0;
-            String[] input2 = br.readLine().split(" ");
-            int x1 = Integer.parseInt(input2[0]);
-            int y1 = Integer.parseInt(input2[1]);
-            int x2 = Integer.parseInt(input2[2]);
-            int y2 = Integer.parseInt(input2[3]);
+          int sum = 0;
+          for(int i = x1; i <= x2; i++) {
+              sum += acc[i][y2] - acc[i][y1 - 1];
+          }
 
-            for(int i = x1; i <= x2; i++) {
-                sum += accRow[i][y2] - accRow[i][y1 - 1];
-            }
-
-            bw.write(sum + "\n");
-        }
-
-        bw.flush();
+          bw.write(sum + "\n");
+      }
+      bw.flush();
     }
 }
