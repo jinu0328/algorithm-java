0b20efb482b9eb2442a71ddc589addd1d0d3ab64
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-11-04 15:46:45 +0900

    [level 1] Title: 폰켓몬, Time: 17.06 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,20 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int N = nums.length; // 포켓몬 수 N/2마리 고를 수 있음
-        Map<Integer, Integer> result = new HashMap<>();
-
-        for(int num : nums) {
-            result.put(num, result.getOrDefault(num, 0) + 1);
+        int possibleNum = nums.length / 2;
+        Set<Integer> categories = new HashSet<>();
+        
+        for(Integer num : nums) {
+            categories.add(num);
         }
-
-        if(result.size() > N / 2) { // Map의 사이즈가 N/2보다 크다면 다 다른 종류로 가능
-            return N / 2;
-        }
-        else {
-            return result.size(); // 아니라면 Map의 사이즈만큼만 가능
-        }
+        
+        return Math.min(possibleNum, categories.size());
     }
 }
