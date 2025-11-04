aec8fafd0f81a330f57f06e253451a4573ab25c1
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-11-05 00:23:08 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 511.01 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 131 MB, 시간: 87.10 ms
+메모리: 87.8 MB, 시간: 511.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 28일 00:09:55
+2025년 11월 05일 00:23:05
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,66 +1,49 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Bridge bridge = new Bridge(bridge_length, weight);
-
-        int truck_index = 0;
-
-        for (int time = 1; ; time++) {
-            bridge.go();
-
-            if (truck_index < truck_weights.length &&
-                bridge.add(new Truck(truck_weights[truck_index]))) {
-                truck_index++;
+        int count = 0;
+        int currentWeight = 0;
+        Queue<Truck> trucks = new LinkedList<>();
+        for(int weights : truck_weights) {
+            trucks.add(new Truck(weights));
+        }
+        List<Truck> running = new ArrayList<>();
+        List<Truck> completed = new ArrayList<>();
+        while(completed.size() < truck_weights.length) {
+            count++;
+            if(!trucks.isEmpty()) {
+                Truck truck = trucks.peek();
+                if(currentWeight + truck.weight <= weight) {
+                    Truck currentTruck = trucks.poll();
+                    running.add(currentTruck);
+                    currentWeight += currentTruck.weight;
+                }
             }
-
-            if (truck_index == truck_weights.length && bridge.trucks.isEmpty()) {
-                return time;
+            
+            for(Truck runningTruck : running) {
+                runningTruck.move();
+                if(runningTruck.position == bridge_length) {
+                    currentWeight -= runningTruck.weight;
+                    completed.add(runningTruck);
+                }
+                
             }
         }
+        
+        return count + 1;
     }
-
-    static class Bridge {
-        int length;
-        int weight;
-        List<Truck> trucks = new ArrayList<>();
-
-        public Bridge(int length, int weight) {
-            this.length = length;
-            this.weight = weight;
-        }
-
-        public boolean add(Truck truck) {
-            if (weight - getTotal() < truck.weight) {
-                return false;
-            }
-            trucks.add(truck);
-            return true;
-        }
-
-        public void go() {
-            trucks.forEach(Truck::go);
-            trucks.removeIf(truck -> truck.distance >= length);
-        }
-
-        private int getTotal() {
-            return trucks.stream()
-                    .mapToInt(t -> t.weight)
-                    .sum();
-        }
-    }
-
+    
     static class Truck {
+        int position = 0;
         int weight;
-        int distance = 0;
-
+        
         public Truck(int weight) {
             this.weight = weight;
         }
-
-        public void go() {
-            distance++;
+        
+        public void move() {
+            position++;
         }
     }
 }
