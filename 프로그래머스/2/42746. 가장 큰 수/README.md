d4639fed4536a0edc078f7470d547e1df79996ae
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-11-06 20:54:11 +0900

    [level 2] Title: 가장 큰 수, Time: 2990.53 ms, Memory: 410 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 135 MB, 시간: 242.72 ms
+메모리: 410 MB, 시간: 2990.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 01일 21:13:06
+2025년 11월 06일 20:54:09
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,22 +1,27 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        // Step 1. 숫자 배열을 문자열 배열로 변환
-        String[] nums = Arrays.stream(numbers)
-                              .mapToObj(String::valueOf)
-                              .toArray(String[]::new);
-
-        // Step 2. 정렬: (b + a)와 (a + b)를 비교해서 더 큰 쪽을 앞에 오도록
-        Arrays.sort(nums, (a, b) -> (b + a).compareTo(a + b));
-
-        // Step 3. 모두 0이면 "0" 반환
-        if (nums[0].equals("0")) {
+        String[] rawNumbers = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            rawNumbers[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(rawNumbers, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        });
+        
+        if(rawNumbers[0].equals("0")) {
             return "0";
         }
-
-        // Step 4. 이어붙이기
-        return String.join("", nums);
+        
+        String result = "";
+        for(String s : rawNumbers) {
+            result += s;
+        }
+        return result;
     }
 }
