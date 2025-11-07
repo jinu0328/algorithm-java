c8ae9f62279d1ef9073b4ae7d7666cbc103a9083
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-11-07 14:07:57 +0900

    [level 2] Title: 소수 찾기, Time: 29.15 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 90.9 MB, 시간: 29.42 ms
+메모리: 84.2 MB, 시간: 29.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 09일 19:18:18
+2025년 11월 07일 14:07:54
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,48 +1,42 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class Solution {
+class Solution {
+    Set<Integer> candidates = new HashSet<>();
+    boolean[] visited;
     public int solution(String numbers) {
-        Set<Integer> numberSet = new HashSet<>();
-        boolean[] visited = new boolean[numbers.length()];
-
-        generateNumber("", numbers, numberSet, visited);
-
-        int count = 0;
-        for(int num : numberSet) {
-            if(isPrime(num)) {
-                count++;
-            }
-        }
-
-        return count;
+        visited = new boolean[numbers.length()];
+        dfs("", numbers);
+    
+        return (int) candidates.stream()
+            .filter(c -> isPrime(c))
+            .count();
+        
     }
-
-    private void generateNumber(String current, String numbers, Set<Integer> numberSet, boolean[] visited) {
-        if(current != "") {
-            numberSet.add(Integer.parseInt(current));
-        }
-
-        for(int i = 0; i < numbers.length(); i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                generateNumber(current + numbers.charAt(i), numbers, numberSet, visited);
-                visited[i] = false;
-            }
-        }
-    }
-
+    
     private boolean isPrime(int num) {
         if(num < 2) {
             return false;
         }
-
+        
         for(int i = 2; i * i <= num; i++) {
             if(num % i == 0) {
                 return false;
             }
         }
-
         return true;
+    }
+    
+    private void dfs(String start, String numbers) {
+        if(!start.isEmpty()) {
+            candidates.add(Integer.parseInt(start));
+        }
+        
+        for(int i = 0; i < numbers.length(); i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                dfs(start + numbers.charAt(i), numbers);
+                visited[i] = false;
+            }
+        }
     }
 }
