1166c65837628c5480846972305b4e8fca9c2969
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-11-07 19:01:22 +0900

    [level 2] Title: 피로도, Time: 3.00 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,23 @@
-import java.util.*;
-
 class Solution {
-    boolean[] isVisited;
-    int answer = 0;
+    int count = 0;
     public int solution(int k, int[][] dungeons) {
-        isVisited = new boolean[dungeons.length];
-        
-        dfs(k, dungeons, 0);
-        return answer;
+        boolean[] visited = new boolean[dungeons.length];
+
+        explore(k, dungeons, visited, 0);
+
+        return count;
+
     }
-    
-    private void dfs(int k, int[][] dungeons, int count) {
+
+    private void explore(int k, int[][] dungeons, boolean[] visited, int depth) {
+        count = Math.max(count, depth);
+        
         for(int i = 0; i < dungeons.length; i++) {
-            if(!isVisited[i] && k >= dungeons[i][0]) {
-                isVisited[i] = true;
-                dfs(k - dungeons[i][1], dungeons, count + 1);
-                isVisited[i] = false;
+            if(!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                explore(k - dungeons[i][1], dungeons, visited, depth + 1);
+                visited[i] = false;
             }
         }
-        answer = Math.max(answer, count);
     }
 }
