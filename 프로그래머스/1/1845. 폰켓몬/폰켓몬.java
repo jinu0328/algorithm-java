1e395366a5f8be1b46f2c80f5361bbd48f937342
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-12-15 00:11:20 +0900

    [level 1] Title: 폰켓몬, Time: 4.43 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,13 +1,14 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
-        int possibleNum = nums.length / 2;
+        int pick = nums.length / 2;
         Set<Integer> categories = new HashSet<>();
         
         for(Integer num : nums) {
             categories.add(num);
         }
         
-        return Math.min(possibleNum, categories.size());
+        return Math.min(pick, categories.size());
     }
 }
