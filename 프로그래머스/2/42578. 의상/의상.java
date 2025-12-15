84255b448bba88ffea31d27496313b79dd1a2c69
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-12-16 02:04:24 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,14 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> categories = new HashMap<>();
+        
         for(String[] cloth : clothes) {
             categories.put(cloth[1], categories.getOrDefault(cloth[1], 0) + 1);
         }
-        Collection<Integer> counts = categories.values();
+        
         int answer = 1;
-        for(Integer count : counts) {
+        for(Integer count : categories.values()) {
             answer *= count + 1;
         }
+        
         return answer - 1;
     }
 }
