cda120cc42969723ff1e4e56255383be34deef65
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-12-16 02:23:26 +0900

    [level 3] Title: 베스트앨범, Time: 5.06 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,61 +1,79 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
+
     public int[] solution(String[] genres, int[] plays) {
-        Musics musics = new Musics();
-        for(int i = 0; i < plays.length; i++) {
-            musics.add(genres[i], new Music(i, plays[i]));
+        Genres myGenres = new Genres();
+
+        for (int i = 0; i < plays.length; i++) {
+            myGenres.add(genres[i], new Music(i, plays[i]));
         }
-        
-        List<Music> topMusics = musics.getTopMusics();
-        
-        return topMusics.stream().mapToInt(music -> music.id).toArray();
+
+        List<Music> topMusics = myGenres.getTopMusics();
+
+        return topMusics.stream()
+                .mapToInt(music -> music.id)
+                .toArray();
     }
-    
-    static class Musics {
-        Map<String, List<Music>> musics = new HashMap<>();
-        
-        public void add(String genre, Music music) {
-            List<Music> musicList = musics.get(genre);
-            if(musicList == null) {
-                musicList = new ArrayList<>();
+
+    static class Genres {
+        private final Map<String, Genre> genres = new HashMap<>();
+
+        public void add(String genreName, Music music) {
+            Genre genre = genres.get(genreName);
+
+            if (genre == null) {
+                genre = new Genre();
+                genres.put(genreName, genre);
             }
-            musicList.add(music);
-            musics.put(genre, musicList);
+
+            genre.add(music);
         }
-        
+
+
+        public List<Music> getTopMusics() {
+            List<Music> result = new ArrayList<>();
+
+            List<Genre> genreList = new ArrayList<>(genres.values());
+
+            genreList.sort((g1, g2) -> g2.totalPlay - g1.totalPlay);
+
+            for (Genre genre : genreList) {
+                List<Music> topMusics = genre.getTopMusics();
+                result.addAll(topMusics);
+            }
+
+            return result;
+        }
+    }
+
+    static class Genre {
+        private int totalPlay = 0;
+        private final List<Music> musics = new ArrayList<>();
+
+        public void add(Music music) {
+            musics.add(music);
+            totalPlay += music.plays;
+        }
+
         public List<Music> getTopMusics() {
             List<Music> topMusics = new ArrayList<>();
-            List<List<Music>> musicLists= new ArrayList<>(musics.values());
-            musicLists.sort(new Comparator<List<Music>>() {
-                @Override
-                public int compare(List<Music> o1, List<Music> o2) {
-                    return o2.stream().mapToInt(music -> music.plays).sum() 
-                        - o1.stream().mapToInt(music -> music.plays).sum();
-                }
-            });
-            for(List<Music> topMusicList : musicLists) {
-                topMusicList.sort(new Comparator<Music>() {
-                    @Override
-                    public int compare(Music o1, Music o2) {
-                        return o2.plays - o1.plays;
-                    }
-                });
-                topMusics.add(topMusicList.get(0));
-                if(topMusicList.size() > 1) {
-                    topMusics.add(topMusicList.get(1));
-                }
+            musics.sort((m1, m2) -> m2.plays - m1.plays);
+            
+            topMusics.add(musics.get(0));
+            
+            if (musics.size() > 1) {
+                topMusics.add(musics.get(1));
             }
             
             return topMusics;
-                
         }
     }
-    
+
     static class Music {
         int id;
         int plays;
+
         public Music(int id, int plays) {
             this.id = id;
             this.plays = plays;
